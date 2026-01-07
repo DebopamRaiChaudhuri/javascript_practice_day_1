7ec1bf9ebab47efc67d14979abfd7d4316102c90
Debopam Rai Chaudhuri <88828926+DebopamRaiChaudhuri@users.noreply.github.com>
2026-01-08 04:37:59 +0530

    new day 1 commit

---
+++ function.js
@@ -0,0 +1,28 @@
+function compliment(name) {
+    console.log("Hey " + name + " you're beautiful" )
+    console.log("Hey " + name + " you're gentle" )
+    console.log("Hey " + name + " I love You" )
+    
+}
+compliment("Niharika")
+compliment("Ankhi")
+
+
+function add(a , b) {
+    console.log( a+b)
+    
+}
+add(10,5)
+
+
+function add(a , b) {
+    return a+b
+}
+
+result1 = add(10,4)
+result2 = add (10,5)
+result3 = add(14,15)
+
+console.log( "The result of this subject 1 is :", result1)
+console.log( "The result of this subject 2 is :", result2)
+console.log( "The result of this subject 3 is :", result3)
---
+++ loops.js
@@ -0,0 +1,39 @@
+// for loop
+// let a = 1;
+
+// for (let i = 0; i < 159; i++) {
+//      console.log (a + i);
+    
+// }
+
+
+//for in loop (used to find keys of elements inside a object)
+// let object ={
+//     name: "Deboapam Rai Chaudhuri",  // key:element
+//     rollid: 1928
+// }
+
+// console.log()
+// for (const key in object) {
+   
+    
+//     const element = object[key];
+//     console.log(key , element)
+    
+    
+// }
+
+// for off loop (used for iterators and array data structures)
+// let object ={
+//     name:"Debopam"
+// }
+// for (const char of "Debopam") {
+//     console.log(char)
+    
+// }
+let n = 15;
+while (n>1) {
+    console.log(n)
+    n--;
+    
+}
